8c26a294401f3aa68d16c1511d9d75d4e5aff1a4
Daniel-V-Logg <st10310998@vcconnect.edu.za>
2025-09-10 22:06:32 +0200

    project completed

---
+++ MainForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MunicipalServiceApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnReportIssues_Click(object sender, EventArgs e)
+        {
+            var reportForm = new ReportIssuesForm();
+            reportForm.ShowDialog();
+        }
+
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Disable future features as per requirements
+            btnLocalEvents.Enabled = false;
+            btnServiceStatus.Enabled = false;
+            
+            // Set tooltips for disabled buttons
+            btnLocalEvents.Text = "Local Events and Announcements\n(Coming Soon)";
+            btnServiceStatus.Text = "Service Request Status\n(Coming Soon)";
+        }
+    }
+}
